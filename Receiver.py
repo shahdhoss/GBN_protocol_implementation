6ae5381f885d1a0b10760e60fc6dc07aedc0a1d2
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-04-22 16:12:08 +0200

    it works nowgit add .git add .

--- Receiver.py
+++ Receiver.py
@@ -1,17 +1,18 @@
 from socket import *
+from PIL import Image
+import PIL
+import io
 def get_packetid(packet):
     packet_id=packet[:2]
     return int.from_bytes(packet_id, byteorder='big')
 def get_data(packet):
     data=packet[4:2044]
-    data_tostring=str(data).strip("b'")
     # print("length of the data in bytes: ", len(data_tostring))
-    return data_tostring
+    return data
 def get_trailing(packet):
     trailing=packet[2044:]
-    trailing_tostring=str(trailing).strip("b'")
     # print("length of the trailing part: ", len(trailing_tostring.encode()))
-    return trailing_tostring
+    return trailing
 server_port=1200
 server_socket= socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(("",server_port))
@@ -20,11 +21,11 @@ data=[]
 packet_ids=[]
 variable=0
 while True:
-    print(variable)
+    # print(variable)
     message, client_address = server_socket.recvfrom(2048)
     variable=variable+1
     packet_id=get_packetid(message)
-    print("this is the packet id:", packet_id)
+    # print("this is the packet id:", packet_id)
     if len(packet_ids)==0:
         packet_ids.append(packet_id)
     if int(packet_id)==int(packet_ids[-1])+1 or len(packet_ids)==1 :         #making sure that its the right packet, before saving its data. Knowing that packetids are numbers in sequence
@@ -36,14 +37,30 @@ while True:
         trailing=get_trailing(message)
         print("this is the trailing: ", trailing)
         message=get_data(message)
-        print("this is the data: ",message)
+        # print("this is the data: ",message)
         data.append(message)
         # print("the list of all data segments: ",data)
-        if(trailing=='0xFFFF'):
-            image_bytes=''
+        if(trailing== b''):
+            # print("im herrre")
+            image_bytes= b''
+            print("image bytes type: ",type(image_bytes))
+            print("image bytes type: ",type(data))
             for i in range(len(data)):
                 image_bytes=image_bytes+data[i]
-            file=open('sent_image.jpeg')
-            file.write()
+            with open('sent_image.jpeg', 'wb') as file:
+                file.write(image_bytes)
+            try:
+                with open('sent_image.jpeg', 'rb') as file:  
+                    binary_stream = io.BytesIO(file.read())
+                    image=Image.open(file)
+                    image.show()
+                print("Image file 'sent_image.jpeg' has been created successfully.")
+            except PIL.UnidentifiedImageError:
+                # print(image_bytes)
+                print("Error: Unable to identify the image file. Make sure the file contains valid image data.")
+            except FileNotFoundError:
+                print("Error: File 'sent_image.jpeg' not found.")
+            except Exception as e:
+                print("An unexpected error occurred:", e)
     else:
         server_socket.sendto(packet_ids[-1].to_bytes(2,byteorder='big'),client_address)   #sending ack
--- Sender.py
+++ Sender.py
@@ -4,8 +4,11 @@ import time
 ReceiverIp = 'localhost'
 ReceiverPort = 1200
 MSS = 2040           #Maximum segment size
-N = 3 #windows size-> how many unacknowledged packets can be in transit at any given time
+N = 3                #windows size-> how many unacknowledged packets can be in transit at any given time
 
+def get_packetid(packet):
+    packet_id=packet[:2]
+    return int.from_bytes(packet_id, byteorder='big')
 def read(filee):
     Data = []
     with open(filee, 'rb') as i:  # rb (binary) - images
@@ -16,8 +19,8 @@ def read(filee):
             Data.append(Chunks)
     return Data
 
-filee= 'Computer Networks Project/small file.jpeg'
-print(len(read(filee)))
+filee= 'Computer Networks Project\large file.jpeg'
+# print(len(read(filee)))
 
 def make_packets(flag, packet_id, data, file_id):
     packet_id = packet_id.to_bytes(2, byteorder='big') 
@@ -27,7 +30,7 @@ def make_packets(flag, packet_id, data, file_id):
     else:    
         trailer_bits = 0x0000
     trailer_bits = trailer_bits.to_bytes(4, byteorder='big')
-    Packet = file_id + packet_id+trailer_bits + data 
+    Packet = packet_id+ file_id + data+ trailer_bits 
     return Packet
 
 def send_to_receiver(packets, ip_address, port):
@@ -70,10 +73,7 @@ def send_to_receiver(packets, ip_address, port):
 
 #     print("Packets are sent successfully!")
 #     SenderSocket.close()
-
-
 # sender(filee, ReceiverPort, ReceiverIp)
-
 
 def sender(filee, receiver_port, ip_address):
     Packets = read(filee)
@@ -85,6 +85,7 @@ def sender(filee, receiver_port, ip_address):
     while SendBase < PacketLength:
         if NextSeqNum < SendBase + N and NextSeqNum < PacketLength:
             Packet = make_packets(NextSeqNum == PacketLength - 1, NextSeqNum, Packets[NextSeqNum], 1)
+            print("this is the packetid inside the while loop: " ,get_packetid(Packet))
             SenderSocket.sendto(Packet, (ip_address, receiver_port))
             print(f"Packet {NextSeqNum} sent..")
             if not Timer:  
@@ -95,7 +96,7 @@ def sender(filee, receiver_port, ip_address):
         try:
             AckPacket, _ = SenderSocket.recvfrom(2048)
             AckNumber = int.from_bytes(AckPacket[:2], byteorder='big')  
-            print("this is the packet no: ",AckNumber)
+            # print("this is the packet no: ", AckNumber)
             if SendBase <= AckNumber < SendBase + N:     
                 SendBase = AckNumber + 1
                 Timer = False
@@ -107,7 +108,6 @@ def sender(filee, receiver_port, ip_address):
                     Packet = make_packets(i == PacketLength - 1, i, Packets[i], 1)
                     SenderSocket.sendto(Packet, (ip_address, receiver_port))
                     print(f"Packet {i} retransmitted..")
-
     print("Packets are sent successfully!")
     SenderSocket.close()
 
