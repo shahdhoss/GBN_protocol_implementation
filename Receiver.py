ee07ad1f0bb7e238060dbb88c94f8d08687a5644
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-05-14 18:54:24 +0300

    everything works nowgit add .!

--- Receiver.py
+++ Receiver.py
@@ -86,8 +86,9 @@ def get_trailing(packet):
     # print("length of the trailing part: ", len(trailing_tostring.encode()))
     return trailing
 server_port=1200
+server_ip='localhost'
 server_socket= socket(AF_INET, SOCK_DGRAM)
-server_socket.bind(("",server_port))
+server_socket.bind((server_ip,server_port))
 print("the server is ready to recieve ")
 data=[]
 packet_ids=[]
@@ -107,59 +108,58 @@ while True:
     # print("this is the packet id:", packet_id)
     if len(packet_ids)==0:
         packet_ids.append(packet_id)
-    if int(packet_id)==int(packet_ids[-1])+1 or len(packet_ids)==1 :         #making sure that its the right packet, before saving its data. Knowing that packetids are numbers in sequence
+    if int(packet_id)==int(packet_ids[-1]) + 1 or len(packet_ids)==1 :         #making sure that its the right packet, before saving its data. Knowing that packetids are numbers in sequence
         if len(packet_ids)==0:
             continue
         # Simulate packet loss
-        # drop_probability = randint(1, 100)
-        # if drop_probability >= 5 and drop_probability <= 15:
-            # print(f"Packet {packet_id} dropped (simulated)")
-            # if packet_id==packet_ids[0]:
-            #   packet_ids=[]
-            # lost_packets.append(packet_id)
-            # continue
+        drop_probability = randint(1, 100)
+        if drop_probability >= 5 and drop_probability <= 15:
+            print(f"Packet {packet_id} dropped (simulated)")
+            if packet_id==packet_ids[0]:
+                packet_ids=[]
+                lost_packets.append(packet_id)
+            continue
         else:
             packet_ids.append(packet_id)
-        # if(len(packet_ids)%N ==0):
-        server_socket.sendto(packet_ids[-1].to_bytes(2,byteorder='big'),client_address) #sending ack
-        hour_time_of_sending= datetime.now().hour
-        minute_time_of_sending= datetime.now().minute
-        second_time_of_sending =datetime.now().second
-        print(f"Packet recieved at: {hour_time_of_sending}:{minute_time_of_sending}:{second_time_of_sending}")
-        trailing=get_trailing(message)
-        # print("this is the trailing: ", trailing)
-        message=get_data(message)
-        # print("this is the data: ",message)
-        data.append(message)
-        # print("the list of all data segments: ",data)
+            # if(len(packet_ids)%N ==0):
+            server_socket.sendto(packet_ids[-1].to_bytes(2,byteorder='big'),client_address)     #sending ack
+            print("packet id:(ack): ",packet_ids[-1])
+            hour_time_of_sending= datetime.now().hour
+            minute_time_of_sending= datetime.now().minute
+            second_time_of_sending =datetime.now().second
+            print(f"Packet recieved at: {hour_time_of_sending}:{minute_time_of_sending}:{second_time_of_sending}")
+            trailing=get_trailing(message)
+            # print("this is the trailing: ", trailing)
+            message=get_data(message)
+            # print("this is the data: ",message)
+            data.append(message)
+            # print("the list of all data segments: ",data)
 
-        received_times.append(time.time())
-        received_packet_ids.append(packet_id)
-        
-
-        if(trailing== b''):
-            # print("im herrre")
-            image_bytes= b''
-            print("image bytes type: ",type(image_bytes))
-            print("image bytes type: ",type(data))
-            for i in range(len(data)):
-                image_bytes=image_bytes+data[i]
-            with open('sent_image.jpeg', 'wb') as file:
-                file.write(image_bytes)
-            try:
-                with open('sent_image.jpeg', 'rb') as file:  
-                    binary_stream = io.BytesIO(file.read())
-                    image=Image.open(file)
-                    image.show()
-                print("Image file 'sent_image.jpeg' has been created successfully.")
-            except PIL.UnidentifiedImageError:
-                # print(image_bytes)
-                print("Error: Unable to identify the image file. Make sure the file contains valid image data.")
-            except FileNotFoundError:
-                print("Error: File 'sent_image.jpeg' not found.")
-            except Exception as e:
-                print("An unexpected error occurred:", e)
-            break
+            received_times.append(time.time())
+            received_packet_ids.append(packet_id)
+            if(trailing== b''):
+                # print("im herrre")
+                image_bytes= b''
+                print("image bytes type: ",type(image_bytes))
+                print("image bytes type: ",type(data))
+                for i in range(len(data)):
+                    image_bytes=image_bytes+data[i]
+                with open('sent_image.jpeg', 'wb') as file:
+                    file.write(image_bytes)
+                try:
+                    with open('sent_image.jpeg', 'rb') as file:  
+                        binary_stream = io.BytesIO(file.read())
+                        image=Image.open(file)
+                        image.show()
+                    print("Image file 'sent_image.jpeg' has been created successfully.")
+                except PIL.UnidentifiedImageError:
+                    # print(image_bytes)
+                    print("Error: Unable to identify the image file. Make sure the file contains valid image data.")
+                except FileNotFoundError:
+                    print("Error: File 'sent_image.jpeg' not found.")
+                except Exception as e:
+                    print("An unexpected error occurred:", e)
+                break
     else:
         server_socket.sendto(packet_ids[-1].to_bytes(2,byteorder='big'),client_address)   #sending ack
 
